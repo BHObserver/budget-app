8a9f16501465353917d7ad4b69b7374f224015da
BHObserver <BHObserver0@gmail.com>
2024-01-30 12:05:22 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :expenses
   has_many :payments, foreign_key: 'author_id'
 
-  validates :name, presence: true
+
 end
