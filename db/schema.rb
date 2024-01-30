78b32ac5662bb8dd77e7db2199948d07f2e52cfb
BHObserver <BHObserver0@gmail.com>
2024-01-30 20:38:19 +0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_30_060008) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
