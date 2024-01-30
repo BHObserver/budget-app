b19ee05583c9a3fb8a18c6e51890ee7914d13ff4
BHObserver <BHObserver0@gmail.com>
2024-01-30 22:35:47 +0600

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Set the default host for Devise mailer in development
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
