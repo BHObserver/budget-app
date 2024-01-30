cfbbd82888fe21861ec14c91f30a4203b0a3ddca
BHObserver <BHObserver0@gmail.com>
2024-01-30 12:04:34 +0600

    Update expense.rb

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,7 +3,7 @@ class Expense < ApplicationRecord
   has_many :allocations
   has_many :payments, through: :allocations
 
-  validates :name, :icon, presence: true
+
   has_one_attached :icon
 
   def total_payment_amount
