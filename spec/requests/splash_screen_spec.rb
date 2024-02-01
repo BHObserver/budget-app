3b2adc5813e53c2284282fb7d5521085360ea64f
BHObserver <BHObserver0@gmail.com>
2024-02-02 02:27:12 +0600

    Update splash_screen_spec.rb

--- spec/requests/splash_screen_spec.rb
+++ spec/requests/splash_screen_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'SplashScreens', type: :request do
   describe 'GET /index' do
-    it 'should be response successull' do
+    before(:each) do
       get unauthenticated_root_path
+    end
+
+    it 'responds successfully' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should render the index template' do
-      get unauthenticated_root_path
+    it 'renders the index template' do
       expect(response).to render_template(:index)
     end
 
-    it 'should include the app name' do
-      get unauthenticated_root_path
+    it 'includes the app name' do
       expect(response.body).to include('FinanceFlick')
     end
   end
