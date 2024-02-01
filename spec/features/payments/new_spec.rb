9cc53ebd25e32a7b9c3369101db4d836d7f6dcd6
BHObserver <BHObserver0@gmail.com>
2024-02-02 02:27:06 +0600

    Update new_spec.rb

--- spec/features/payments/new_spec.rb
+++ spec/features/payments/new_spec.rb
@@ -2,31 +2,15 @@ require 'rails_helper'
 
 RSpec.feature 'Payments::News', type: :feature do
   before(:each) do
-    @user = User.create(
-      id: 2,
-      name: 'becky',
-      email: 'becky@mail.com',
-      password: 'abcxyz123',
-      confirmed_at: Time.now
-    )
-    @expense1 = Expense.create(
-      name: 'Expense 2',
-      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'icon.png'), 'image/png'),
-      user_id: @user.id
-    )
+    @user = create_user
+    @expense1 = create_expense(name: 'Expense 2', icon_path: 'icon.png', user: @user)
 
-    visit new_user_session_path
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
-    click_button 'Log in'
+    log_in_user(@user)
     visit new_expense_payment_path(@expense1)
   end
 
   scenario 'User creates a new payment with valid data' do
-    fill_in 'payment_name', with: 'Test Payment'
-    fill_in 'payment_amount', with: 100.00
-
-    click_button 'Create Transaction'
+    fill_in_and_submit_payment_form('Test Payment', 100.00)
 
     expect(page).to have_content('New Transaction')
   end
@@ -35,5 +19,36 @@ RSpec.feature 'Payments::News', type: :feature do
     click_button 'Create Transaction'
 
     expect(page).to have_content('New Transaction')
+  end
+
+  def create_user
+    User.create(
+      id: 2,
+      name: 'becky',
+      email: 'becky@mail.com',
+      password: 'abcxyz123',
+      confirmed_at: Time.now
+    )
+  end
+
+  def create_expense(name:, icon_path:, user:)
+    Expense.create(
+      name: name,
+      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', icon_path), 'image/png'),
+      user_id: user.id
+    )
+  end
+
+  def log_in_user(user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+
+  def fill_in_and_submit_payment_form(name, amount)
+    fill_in 'payment_name', with: name
+    fill_in 'payment_amount', with: amount
+    click_button 'Create Transaction'
   end
 end
