651056f654c2d444545f8b6192a82183f0f27da2
BHObserver <BHObserver0@gmail.com>
2024-02-02 02:27:02 +0600

    Update new_spec.rb

--- spec/features/expenses/new_spec.rb
+++ spec/features/expenses/new_spec.rb
@@ -2,30 +2,15 @@ require 'rails_helper'
 
 RSpec.feature 'Expenses::News', type: :feature do
   before(:each) do
-    @user = User.create(
-      id: 2,
-      name: 'becky',
-      email: 'becky@mail.com',
-      password: 'abcxyz123',
-      confirmed_at: Time.now
-    )
-    @expense1 = Expense.create(
-      name: 'Expense 2',
-      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'icon.png'), 'image/png'),
-      user_id: @user.id
-    )
+    @user = create_user
+    @expense1 = create_expense(user: @user)
 
-    visit new_user_session_path
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
-    click_button 'Log in'
+    log_in_user(@user)
     visit new_expense_path
   end
 
   scenario 'User creates a new expense with valid data' do
-    fill_in 'expense_name', with: 'Test Expense'
-    attach_file 'expense_icon', Rails.root.join('spec', 'fixtures', 'files', 'icon.png')
-    click_button 'Save'
+    fill_in_and_submit_expense_form('Test Expense', Rails.root.join('spec', 'fixtures', 'files', 'icon.png'))
 
     expect(page).to have_content('Add Category')
   end
@@ -34,5 +19,36 @@ RSpec.feature 'Expenses::News', type: :feature do
     click_button 'Save'
 
     expect(page).to have_content('New Category')
+  end
+
+  def create_user
+    User.create(
+      id: 2,
+      name: 'becky',
+      email: 'becky@mail.com',
+      password: 'abcxyz123',
+      confirmed_at: Time.now
+    )
+  end
+
+  def create_expense(user:)
+    Expense.create(
+      name: 'Expense 2',
+      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'icon.png'), 'image/png'),
+      user_id: user.id
+    )
+  end
+
+  def log_in_user(user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+
+  def fill_in_and_submit_expense_form(name, icon_path)
+    fill_in 'expense_name', with: name
+    attach_file 'expense_icon', icon_path
+    click_button 'Save'
   end
 end
