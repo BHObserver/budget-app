0d6a73aaff1bd36fc3d1c97019eedb05f36a5681
BHObserver <BHObserver0@gmail.com>
2024-02-02 02:27:00 +0600

    Update index_spec.rb

--- spec/features/expenses/index_spec.rb
+++ spec/features/expenses/index_spec.rb
@@ -2,23 +2,10 @@ require 'rails_helper'
 
 RSpec.feature 'Expenses::Indices', type: :feature do
   before(:each) do
-    @user = User.create(
-      id: 2,
-      name: 'becky',
-      email: 'becky@mail.com',
-      password: 'abcxyz123',
-      confirmed_at: Time.now
-    )
-    @expense1 = Expense.create(
-      name: 'Expense 2',
-      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'icon.png'), 'image/png'),
-      user_id: @user.id
-    )
+    @user = create_user
+    @expense1 = create_expense(user: @user)
 
-    visit new_user_session_path
-    fill_in 'Email', with: @user.email
-    fill_in 'Password', with: @user.password
-    click_button 'Log in'
+    log_in_user(@user)
     visit expenses_path
   end
 
@@ -26,5 +13,30 @@ RSpec.feature 'Expenses::Indices', type: :feature do
     expect(page).to have_content(@expense1.name)
     expect(page).to have_content(@expense1.total_payment_amount)
     expect(page).to have_link('Add Category', href: new_expense_path)
+  end
+
+  def create_user
+    User.create(
+      id: 2,
+      name: 'becky',
+      email: 'becky@mail.com',
+      password: 'abcxyz123',
+      confirmed_at: Time.now
+    )
+  end
+
+  def create_expense(user:)
+    Expense.create(
+      name: 'Expense 2',
+      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'icon.png'), 'image/png'),
+      user_id: user.id
+    )
+  end
+
+  def log_in_user(user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
   end
 end
