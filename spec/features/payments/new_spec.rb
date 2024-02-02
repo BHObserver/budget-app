a8990a88bed1518e1572dd764b5c0b3c1e7fb171
BHObserver <BHObserver0@gmail.com>
2024-02-03 02:33:07 +0600

    Update new_spec.rb

--- spec/features/payments/new_spec.rb
+++ spec/features/payments/new_spec.rb
@@ -19,21 +19,6 @@ RSpec.feature 'Payments::News', type: :feature do
     )
   end
 
-  def create_expense(name:, icon_path:, user:)
-    Expense.create(
-      name:,
-      icon: fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', icon_path), 'image/png'),
-      user_id: user.id
-    )
-  end
-
-  def log_in_user(user)
-    visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-  end
-
   def fill_in_and_submit_payment_form(name, amount)
     fill_in 'payment_name', with: name
     fill_in 'payment_amount', with: amount
