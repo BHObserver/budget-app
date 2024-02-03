1d6bfd1ff3dac12563cdec6d075f32b3a4ad06aa
BHObserver <BHObserver0@gmail.com>
2024-02-04 05:29:50 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :expenses
   has_many :payments, foreign_key: 'author_id'
